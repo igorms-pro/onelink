7402347fd865b4903dd6496f9ab72e8fe8658033
igorms-pro <igorms.pro@gmail.com>
2025-11-06 22:32:15 +0100

    feat: add dark mode background image for auth page

--- apps/web/src/routes/Auth.tsx
+++ apps/web/src/routes/Auth.tsx
@@ -15,23 +15,27 @@ export default function Auth() {
 
   return (
     <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors relative">
-      {/* Subtle grid pattern */}
+      {/* Background image - light mode */}
       <div
-        className="absolute inset-0 opacity-[0.02] dark:opacity-[0.05] pointer-events-none"
+        className="absolute inset-0 pointer-events-none dark:hidden"
         style={{
-          backgroundImage:
-            "linear-gradient(to right, #000 1px, transparent 1px), linear-gradient(to bottom, #000 1px, transparent 1px)",
-          backgroundSize: "20px 20px",
+          backgroundImage: "url(/screen.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       ></div>
 
-      {/* Purple blob gradients */}
-      <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        <div className="absolute top-0 left-0 w-96 h-96 bg-purple-400/10 dark:bg-purple-600/5 rounded-full blur-3xl"></div>
-        <div className="absolute top-1/4 right-0 w-80 h-80 bg-purple-300/8 dark:bg-purple-500/4 rounded-full blur-3xl"></div>
-        <div className="absolute bottom-1/4 left-1/4 w-72 h-72 bg-purple-200/6 dark:bg-purple-400/3 rounded-full blur-3xl"></div>
-        <div className="absolute bottom-0 right-1/4 w-64 h-64 bg-purple-300/8 dark:bg-purple-500/4 rounded-full blur-3xl"></div>
-      </div>
+      {/* Background image - dark mode */}
+      <div
+        className="absolute inset-0 pointer-events-none hidden dark:block"
+        style={{
+          backgroundImage: "url(/screen-dark.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
 
       <HeaderMobileSignIn />
       <main className="flex-1 mx-auto max-w-md w-full p-6 flex flex-col justify-center relative z-10">
