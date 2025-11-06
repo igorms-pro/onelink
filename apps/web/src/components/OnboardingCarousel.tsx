5d3cf3113d0c9c16afabec9a24224fcd1bb9f4d2
igorms-pro <igorms.pro@gmail.com>
2025-11-06 22:37:12 +0100

    fix: make theme and language buttons clickable in onboarding

--- apps/web/src/components/OnboardingCarousel.tsx
+++ apps/web/src/components/OnboardingCarousel.tsx
@@ -82,23 +82,25 @@ export function OnboardingCarousel({ onComplete }: OnboardingCarouselProps) {
     <div className="fixed inset-0 z-50 flex flex-col">
       {/* Background image - light mode */}
       <div
-        className="fixed inset-0 dark:hidden"
+        className="fixed inset-0 dark:hidden pointer-events-none"
         style={{
           backgroundImage: "url(/screen.png)",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          zIndex: 0,
         }}
       ></div>
 
       {/* Background image - dark mode */}
       <div
-        className="fixed inset-0 hidden dark:block"
+        className="fixed inset-0 hidden dark:block pointer-events-none"
         style={{
           backgroundImage: "url(/screen-dark.png)",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          zIndex: 0,
         }}
       ></div>
 
